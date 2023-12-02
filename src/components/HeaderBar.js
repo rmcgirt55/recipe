7e8e68ac830b84c17502670ba7681ff97b6c5a82
Richard McGirt <richardmcgirt@Richards-Mac-mini.local>
2023-12-02 09:50:53 -0500

    reformated the header bar

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,154 +1,67 @@
-import { useState } from "react";
-import { Container } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import { useContext } from "react";
 import { useNavigate } from "react-router";
-import Card from 'react-bootstrap/Card';
+import { CurrentUser } from "../contexts/CurrentUser";
 
-function NewRecipe() {
+function HeaderBar() {
+  const navigate = useNavigate();
 
-	const navigate = useNavigate()
+  const { currentUser, setCurrentUser } = useContext(CurrentUser);
+  console.log(currentUser)
+    const token = localStorage.getItem("token");
+    const user_interface = () =>
+    {
+        //if token then logout button else login and singup button 
+        if(token)
+        {
+            return(
+                <button variant="outline-success" onClick={logout}>Logout</button>
+            )
+        }
+        else
+        {
+            return(
+                <>
+                <button variant="outline-success" onClick={() => navigate("/sign-up")}>Sign Up</button>
+                <button variant="outline-success" onClick={() => navigate("/login")}>Login</button>
+                </>
+            )
+        }
+    }
 
-	const [recipe, setRecipe] = useState({
-		recipename: '',
-		image: '',
-		cuisines: '',
-		difficulty: '',
-		preptime: '',
-		cooktime: '',
-		ingredients: '',
-		directions: '',
-		description: ''
-	})
+  function logout(e) {
+    localStorage.removeItem("token");
+    setCurrentUser(null)
+    navigate("/");
+  }
 
-	async function handleSubmit(e) {
-		e.preventDefault()
-		const response = await fetch(`https://recipesharingbackend.herokuapp.com/api/recipes`, {
-			method: 'POST',
-			headers: {
-				'x-auth-token': localStorage.getItem('token'),
-				'Content-Type': 'application/json'
-
-			},
-			body: JSON.stringify(recipe)
-		})
-		const data = await response.json()
-		navigate('/recipes')
-	}
+  // this collection of html should be inside of an unordered/ordered list
+  let loginActions =  user_interface();
 
 
-	return (
+  if (currentUser) {
+    loginActions = (
+      <>
+     
+        <div>
+        <button className="login" onClick={logout}>Logout</button>
+        <h6>Logged in as {currentUser.username}</h6>
+        </div>
+        </>
+    );
+  }
+  
+  return (
 
-		<Container>
-			<Card>
-				<Card.Body>
-					<main>
-						<h1 className="text-center">Add a New Recipe</h1>
-						<form onSubmit={handleSubmit}>
-							<div className="form-group ">
-								<label htmlFor="recipename">Recipe Name</label>
-								<input
-									required
-									value={recipe.recipename}
-									onChange={e => setRecipe({ ...recipe, recipename: e.target.value })}
-									className="form-control"
-									id="recipename"
-									name="recipename"
-								/>
-								<div className="form-group">
-									<label htmlFor="image">Enter image URL</label>
-									<input
-										required
-										value={recipe.image}
-										onChange={e => setRecipe({ ...recipe, image: e.target.value })}
-										className="form-control"
-										id="image"
-										name="image"
-									/>
-								</div>
-								<div className="form-group">
-									<label htmlFor="cuisines">Cuisine</label>
-									<input
-										required
-										value={recipe.cuisines}
-										onChange={e => setRecipe({ ...recipe, cuisines: e.target.value })}
-										className="form-control"
-										id="cuisines"
-										name="cuisines"
-									/>
-								</div>
-								<div className="form-group">
-									<label htmlFor="difficulty">Difficulty</label>
-									<input
-										required
-										value={recipe.difficulty}
-										onChange={e => setRecipe({ ...recipe, difficulty: e.target.value })}
-										className="form-control"
-										id="difficulty"
-										name="difficulty"
-									/>
-								</div>
-								<div className="form-group">
-									<label htmlFor="preptime">Preptime</label>
-									<input
-										required
-										value={recipe.preptime}
-										onChange={e => setRecipe({ ...recipe, preptime: e.target.value })}
-										className="form-control"
-										id="preptime"
-										name="preptime"
-									/>
-								</div>
-								<div className="form-group">
-									<label htmlFor="cooktime">Cooktime</label>
-									<input
-										required
-										value={recipe.cooktime}
-										onChange={e => setRecipe({ ...recipe, cooktime: e.target.value })}
-										className="form-control"
-										id="cooktime"
-										name="cooktime"
-									/>
-								</div>
-								<div className="form-group">
-									<label htmlFor="directions">Directions</label>
-									<input
-										required
-										value={recipe.directions}
-										onChange={e => setRecipe({ ...recipe, directions: e.target.value })}
-										className="form-control"
-										id="directions"
-										name="directions"
-									/>
-								</div>
-								<div className="form-group">
-									<label htmlFor="ingredients">Ingredients</label>
-									<input
-										required
-										value={recipe.ingredients}
-										onChange={e => setRecipe({ ...recipe, ingredients: e.target.value })}
-										className="form-control"
-										id="ingredients"
-										name="ingredients"
-									/>
-								</div>
-								<div className="form-group">
-									<label htmlFor="description">Description</label>
-									<input
-										required
-										value={recipe.description}
-										onChange={e => setRecipe({ ...recipe, description: e.target.value })}
-										className="form-control"
-										id="description"
-										name="description"
-									/>
-								</div>
-							</div>
-							<input className="addrecipebtn" type="submit" value="Add Recipe" />
-						</form>
-					</main>
-				</Card.Body>
-			</Card>
-		</Container>
-	)
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        
+        <Navbar.Brand className="text-wrap"><b>"CulinaryCanvas""</b></Navbar.Brand>
+        {loginActions}
+      </Container>
+    </Navbar>
+  );
 }
 
-export default NewRecipe
+export default HeaderBar;
